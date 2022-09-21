2a794bf40c22a713541a4b3b1cff907ab6060ebd
Raquel Custodio <raquel_custodio7@hotmail.com>
2022-09-21 12:52:45 +0100

    Ficha 3 - ex3

--- Ti827ManipulaStrings/src/ti827manipulastrings/ExerciciosStrings.java
+++ Ti827ManipulaStrings/src/ti827manipulastrings/ExerciciosStrings.java
@@ -3,14 +3,53 @@ package ti827manipulastrings;
 
 
 class ExerciciosStrings {
+    static String s,s1;
 
-    static int contaCaracteres() {
-        String s,s1;
-        System.out.println("Insira um texto");
+    static void LeStrings(String texto){
+        System.out.println(texto);
         s = Ti827ManipulaStrings.ler.next();
         s1 = Ti827ManipulaStrings.ler.nextLine();
         s = s+s1;
-        return s.length();
     }
     
-}
+    static int contaCaracteres() {
+       LeStrings("Insira um texto");
+        return s.length();
+    }
+
+    static void arvoreCharAt() {
+      int x,y;
+      LeStrings("Insira uma palavra");
+        for (x=0;x<s.length();x++){
+            for(y=0;y<=x;y++){
+                System.out.print(s.charAt(y));
+            }
+        System.out.println();
+        for (x=s.length()-1; x>=0;x--){
+            for (y=0; y<x;y++){
+                System.out.print(s.charAt(y));
+            }
+        System.out.println();
+        }
+        }
+    }
+
+    static void capitalizaLetras() {
+         
+        LeStrings ("Insira uma palavra: ");
+        System.out.println(s.toLowerCase());
+        LeStrings ("Insira outra palavra: ");
+        System.out.println(s.toUpperCase());
+      
+       
+        
+                
+
+    
+} 
+       
+}   
+    
+      
+
+    
--- Ti827ManipulaStrings/src/ti827manipulastrings/Ti827ManipulaStrings.java
+++ Ti827ManipulaStrings/src/ti827manipulastrings/Ti827ManipulaStrings.java
@@ -17,6 +17,9 @@ public class Ti827ManipulaStrings {
         do{
             System.out.println("-------MENU------");
             System.out.println("1- Tamanho String");
+            System.out.println("2- Árvore de Letras");
+             System.out.println("3- Capitalização de Letras");
+            
             
             System.out.println("0- Sair");
             System.out.println("Insira Opção pretendida");
@@ -28,6 +31,9 @@ public class Ti827ManipulaStrings {
                     System.out.println("A string inserida tem: "+
                     ExerciciosStrings.contaCaracteres()+"caracteres");
                     break;
+                case 2: ExerciciosStrings.arvoreCharAt(); break;
+                case 3: ExerciciosStrings.capitalizaLetras(); break;
+               
                 default:
                    System.out.println("Insira Opção válida");
             }
